3f0c02dd5b38a4187be92489bc8ca298787d92e8
jp-lira01 <jplffaculdade@gmail.com>
2026-04-27 22:02:58 -0300

    Atualização CSS + responsividade e criação do menu lateralpara mobile

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+    const menu = document.querySelector(".menu-lateral")
+    menu.style.display = "flex"
+}
+
+function fecharMenu() {
+    const menu = document.querySelector(".menu-lateral")
+    menu.style.display = "none"
+}
